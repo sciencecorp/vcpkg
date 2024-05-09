394fbb5fd94ff7fe02b0cea660bafa0bbfeb13c5
Antonia Elsen <antoniaelsen@gmail.com>
2024-05-09 15:56:56 -0700

    feat(arm64-osx-clang18): add arm64-osx-clang18 triplet

---
+++ scripts/cmake/toolchain-clang18.cmake
@@ -0,0 +1,8 @@
+
+set(CMAKE_C_COMPILER /opt/homebrew/Cellar/llvm/18.1.5/bin/clang)
+set(CMAKE_CXX_COMPILER /opt/homebrew/Cellar/llvm/18.1.5/bin/clang++)
+set(CMAKE_CXX_COMPILER_ID Clang)
+
+message(STATUS "clang-18 toolchain CMAKE_C_COMPILER = ${CMAKE_C_COMPILER}")
+message(STATUS "clang-18 toolchain CMAKE_CXX_COMPILER = ${CMAKE_CXX_COMPILER}")
+message(STATUS "\n")
