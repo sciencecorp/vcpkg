2031d303df43189438620e6cbc27dbf6bb721fd7
Antonia Elsen <antoniaelsen@gmail.com>
2024-05-09 11:20:01 -0700

    fix(synapse): update ref to use synapse with cli and test flags

--- ports/synapse/portfile.cmake
+++ ports/synapse/portfile.cmake
@@ -1,12 +1,21 @@
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    cli             WITH_CLI
+    tests           WITH_TESTS
+)
+
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:sciencecorp/synapse.git
-    REF "475b3711fc255b1b8ebbca51a67e39db93668b0f"
+    REF "815ac5e06051b3d16f35344f0cd43b79c100a02d"
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DSYNAPSE_BUILD_CLI=${WITH_CLI}
+        -DSYNAPSE_BUILD_TESTS=${WITH_TESTS}
 )
 
 vcpkg_cmake_install()
