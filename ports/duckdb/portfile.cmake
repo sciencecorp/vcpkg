a4c024e7b6b715868425a2b532c3681cb0c9387b
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-03 13:16:58 -0800

    refactor(duckdb): switch portfile to archive download

--- ports/duckdb/portfile.cmake
+++ ports/duckdb/portfile.cmake
@@ -1,214 +1,79 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO duckdb/duckdb
-    REF v${VERSION}
-    HEAD_REF main
-    SHA512 13c67623fc3cc38e7c2e3986b5a3c7d212a8b315a8914ce9b3777bbdac5cb988c956b0a1a698b0af6e7c7211ca7130ed01595c78c92055bfa52ab85e358fe52a
-)
-
-vcpkg_check_features(
-  OUT_FEATURE_OPTIONS D_BUILD_FLAGS
-  FEATURES
-    benchmark                 BUILD_BENCHMARK
-    jdbc                      BUILD_JDBC
-    odbc                      BUILD_ODBC
-    python                    BUILD_PYTHON
-    shell                     BUILD_SHELL
-)
-
-vcpkg_check_features(
-  OUT_FEATURE_OPTIONS D_DEBUG_FLAGS
-  FEATURES
-    crash-on-assert           CRASH_ON_ASSERT
-    debug-stacktrace          DEBUG_STACKTRACE
-    destroy-unpinned-blocks   DESTROY_UNPINNED_BLOCKS
-    disable-unity-build       DISABLE_UNITY
-    disable-sanitizer         DISABLE_SANITIZER
-    disable-memory-safety     DISABLE_MEMORY_SAFETY
-    disable-string-inline     DISABLE_STRING_INLINE
-)
-
-vcpkg_check_features(
-  OUT_FEATURE_OPTIONS D_EXTENSION_FLAGS
-  FEATURES
-    autocomplete              BUILD_AUTOCOMPLETE
-    fts                       BUILD_FTS
-    httpfs                    BUILD_HTTPFS
-    icu                       BUILD_ICU
-    inet                      BUILD_INET
-    jemalloc                  BUILD_JEMALLOC
-    json                      BUILD_JSON
-    parquet                   BUILD_PARQUET
-    sqlsmith                  BUILD_SQLSMITH
-    tpch                      BUILD_TPCH
-    tpcds                     BUILD_TPCDS
-)
-
-# Clean VCPKG feature flags
-# Remove -D and convert ON to 1, add to out list
-function(clean_flags in_list out_list)
-    set(temp_list "")
-    foreach(flag IN LISTS in_list)
-        string(REGEX REPLACE "^-D" "" flag "${flag}")
-
-        if(flag MATCHES "^(.+)=(.+)$")
-            set(flag_name "${CMAKE_MATCH_1}")
-            set(flag_value "${CMAKE_MATCH_2}")
-
-            if(flag_value STREQUAL "ON")
-                set(flag_value "1")
-                list(APPEND temp_list "${flag_name}=${flag_value}")
-            endif()
+if(VCPKG_TARGET_IS_OSX)
+    if (VERSION STREQUAL "1.1.0")
+        set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-osx-universal.zip")
+        set(DUCKDB_SHA512 "7badf2548188e108821a5f4da73ae649c94c994b2808051024ec103f7fbaf7cc16ffc64dff2b3fdb4dcaf9b2dc4734e6bed6058dfa5dd60d1f138ee9c0d02c50")
+    elseif (VERSION STREQUAL "1.1.3")
+        set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-osx-universal.zip")
+        set(DUCKDB_SHA512 "b3b8cdc9a61e088abb131155f33a2463ed7b686581c2565879571f8cc0f2776f2239477277c17744c79c88fdf513c5892b31700027aae9e0fbe7d2eceace1ebf")
+    endif()
+elseif(VCPKG_TARGET_IS_LINUX)
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        if (VERSION STREQUAL "1.1.0")
+            set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-linux-aarch64.zip")
+            set(DUCKDB_SHA512 "fc163d12f7b2f0bd635f239eb400545f87ae62cead38d178755d4947bfebe268a143485457197561edc3873ded67e0f78c660b79cf9bdf19227856989dc69dff")
+        elseif (VERSION STREQUAL "1.1.3")
+            set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-linux-aarch64.zip")
+            set(DUCKDB_SHA512 "c0fe1a95c2c74980645319cabcc7cbb4e80a9bc5517cfa1ef05e32ab9f09a6576d1624db01f7fdd9424862de686cb3269e30f0408b09273faffa8360476b0d73")
         endif()
-    endforeach()
-
-    set(${out_list} ${temp_list} PARENT_SCOPE)
-endfunction()
-
-clean_flags(${D_BUILD_FLAGS} BUILD_FLAGS)
-clean_flags(${D_DEBUG_FLAGS} DEBUG_FLAGS)
-clean_flags(${D_EXTENSION_FLAGS} EXTENSIONS)
-
-set(OTHER_FLAGS)
-list(APPEND OTHER_FLAGS "OVERRIDE_GIT_DESCRIBE=v${VERSION}")
-
-# Recommended to disable vptr sanitizer on arm64-osx
-# https://github.com/duckdb/duckdb/blob/f5ab7c167ee895ecc3fc2cd22b7443fcc60419a2/CMakeLists.txt#L189
-if (TARGET_TRIPLET STREQUAL "arm64-osx")
-    list(APPEND OTHER_FLAGS "DISABLE_SANITIZER=1")
-endif()
-
-vcpkg_configure_make(
-    SOURCE_PATH "${SOURCE_PATH}"
-    COPY_SOURCE
-    SKIP_CONFIGURE
-)
-
-# DuckDB build instructions
-#
-# https://duckdb.org/docs/dev/building/build_instructions
-# https://duckdb.org/docs/dev/building/building_extensions
-#
-# set the environment variables
-# - GEN=ninja
-# - BUILD_<flag>=1 (for all enabled build flags)
-# - BUILD_<ext>=1 (for all enabled extensions)
-# - <flag>=1 (for all enabled debug flags)
-#
-# then call `make <target>`, where target is 'release' or 'debug'
-#
-# this Makefile calls CMake under the hood to build files to build/release or build/debug
-#
-# Because duckdb does not recommend calling cmake configure / build directly,
-#   bits of the following script were adapted from
-#   https://github.com/microsoft/vcpkg/blob/master/scripts/cmake/vcpkg_build_make.cmake
-#   (make flags, build types, etc...)
-#
-
-if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(BUILD_TARGET "debug")
+    else()
+        if (VERSION STREQUAL "1.1.0")
+            set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-linux-amd64.zip")
+            set(DUCKDB_SHA512 "bfce08fa9d5e6b0c0f81968ed3fa415b6e262fea8bf8fa3dd2981ba9f854295afe6ba0167f1941476aef8354935b2254224bf3b56fce330c2aed3e606e3a1c0b")
+        elseif (VERSION STREQUAL "1.1.3")
+            set(DUCKDB_URL "https://github.com/duckdb/duckdb/releases/download/v${VERSION}/libduckdb-linux-amd64.zip")
+            set(DUCKDB_SHA512 "214add5d768d0963a1882d036802a6a3272f3baff4b3832b324e2dc660b76e1b29495b541d8ba71d39c33a0f38e068cac8487e1f06a81a310a96be9e3b39fc00")
+        endif()
+    endif()
 else()
-    set(BUILD_TARGET "release")
+    message(FATAL_ERROR "Unsupported platform")
 endif()
 
-find_program(make_command make REQUIRED)
-
-vcpkg_list(SET make_opts V=1 -j ${VCPKG_CONCURRENCY} -f Makefile)
-z_vcpkg_configure_make_common_definitions()
-
-vcpkg_list(SET no_parallel_make_opts V=1 -j 1 -f Makefile)
-
-foreach(buildtype IN ITEMS "debug" "release")
-    if (buildtype STREQUAL "debug" AND _VCPKG_MAKE_NO_DEBUG)
-        continue()
-    endif()
-
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "${buildtype}")
-        string(TOUPPER "${buildtype}" cmake_buildtype)
-        string(TOLOWER "${buildtype}" target)
-        set(short_buildtype "${short_name_${cmake_buildtype}}")
-        set(path_suffix "${path_suffix_${cmake_buildtype}}")
-
-        set(ENV{GEN} "ninja")
-
-        # Specify CMAKE_INSTALL_PREFIX to vcpkg's package directory
-        #  via DuckDB's Makefile's COMMON_CMAKE_VARS
-        #  which are passed to CMake via the Makefile
-        #  https://github.com/duckdb/duckdb/blob/v1.0.0/Makefile
-        vcpkg_list(SET make_vars)
-        if (buildtype STREQUAL "debug")
-            vcpkg_list(APPEND make_vars "COMMON_CMAKE_VARS=-DCMAKE_INSTALL_PREFIX=${CURRENT_PACKAGES_DIR}/debug")
-        else()
-            vcpkg_list(APPEND make_vars "COMMON_CMAKE_VARS=-DCMAKE_INSTALL_PREFIX=${CURRENT_PACKAGES_DIR}")
-        endif()
-
-        # Configure and build
-
-        set(working_directory ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${short_buildtype})
-        file(MAKE_DIRECTORY "${working_directory}")
-
-        vcpkg_list(SET make_args ${make_opts} ${target} ${make_vars} ${BUILD_FLAGS} ${DEBUG_FLAGS} ${EXTENSIONS} ${OTHER_FLAGS})
-        vcpkg_list(SET no_parallel_make_args ${no_parallel_make_opts} ${target} ${make_vars} ${BUILD_FLAGS} ${DEBUG_FLAGS} ${EXTENSIONS} ${OTHER_FLAGS})
-        vcpkg_execute_build_process(
-            COMMAND ${make_command} ${make_args}
-            NO_PARALLEL_COMMAND ${no_parallel_make_args}
-            WORKING_DIRECTORY ${working_directory}
-            LOGNAME "build-${TARGET_TRIPLET}-${short_buildtype}"
-        )
-
-        set(build_path "${working_directory}/build/${target}")
-
-        # Register extensions
-
-        message(STATUS "Installing extensions")
-        foreach(ext_flag IN ITEMS ${EXTENSIONS})
-            string(REGEX REPLACE "^BUILD_([^=]+).*$" "\\1" ext_name "${ext_flag}")
-            string(REGEX REPLACE "^BUILD_[^=]+=(.*)$" "\\1" ext_value "${ext_flag}")
-            string(TOLOWER ${ext_name} ext_name)
-
-            if(${ext_value})
-                string(TOLOWER ${ext_name} ext_name)
-                message(STATUS "- installing ${ext_name} extension")
-
-                set(ext_path "${build_path}/extension/${ext_name}/${ext_name}.duckdb_extension")
-
-                # duckdb -c "INSTALL 'extension/<ext>/<ext>.duckdb_extension'
-                vcpkg_execute_build_process(
-                    COMMAND "${build_path}/duckdb" -c "INSTALL '${ext_path}';"
-                    WORKING_DIRECTORY "${working_directory}"
-                    LOGNAME "register-extensions-${TARGET_TRIPLET}-${short_buildtype}"
-                )
-            endif()
-        endforeach()
-
-        # Install build
-
-        vcpkg_execute_build_process(
-            COMMAND
-                "${CMAKE_COMMAND}" --build . --target install
-            NO_PARALLEL_COMMAND
-                "${CMAKE_COMMAND}" --build . --target install
-            WORKING_DIRECTORY "${build_path}"
-            LOGNAME "install-${TARGET_TRIPLET}-${short_buildtype}"
-        )
-
-    endif()
-endforeach()
-
-# Clean up installation
-vcpkg_cmake_config_fixup(
-  PACKAGE_NAME duckdb
-  CONFIG_PATH lib/cmake/DuckDB
+vcpkg_download_distfile(
+    ARCHIVE
+    URLS ${DUCKDB_URL}
+    FILENAME "duckdb-${VERSION}-${VCPKG_TARGET_ARCHITECTURE}.zip"
+    SHA512 ${DUCKDB_SHA512}
 )
-# Remove empty dirs
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/duckdb/storage/serialization")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/duckdb/main/capi/header_generation/apis")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/duckdb/main/capi/header_generation/functions")
 
-# License
-vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE)
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    NO_REMOVE_ONE_LEVEL
+)
 
-# Usage
+file(INSTALL "${SOURCE_PATH}/duckdb.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+message(STATUS "Installing DuckDB library to ${CURRENT_PACKAGES_DIR}/lib from ${SOURCE_PATH}")
+if(VCPKG_TARGET_IS_OSX)
+    file(INSTALL "${SOURCE_PATH}/libduckdb.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    file(INSTALL "${SOURCE_PATH}/libduckdb.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+else()
+    file(INSTALL "${SOURCE_PATH}/libduckdb.so" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    file(INSTALL "${SOURCE_PATH}/libduckdb.so" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+endif()
+
+# Install CMake config files
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/duckdb/cmake")
+
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/cmake/Config.cmake.in"
+    "${CURRENT_PACKAGES_DIR}/share/duckdb/cmake/DuckDBConfig.cmake"
+    @ONLY
+)
+
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/cmake/Targets.cmake.in"
+    "${CURRENT_PACKAGES_DIR}/share/duckdb/cmake/DuckDBTargets.cmake"
+    @ONLY
+)
+
+vcpkg_download_distfile(
+    LICENSE_FILE
+    URLS "https://github.com/duckdb/duckdb/blob/v${VERSION}/LICENSE"
+    FILENAME "duckdb-${VERSION}-LICENSE"
+    SKIP_SHA512
+)
+
+vcpkg_install_copyright(FILE_LIST "${LICENSE_FILE}")
+
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
