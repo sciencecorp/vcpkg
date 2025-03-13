0d5c3f537ec5fe75d0fd6cc4e7b07ff0ea0a0d69
Antonia Elsen <antoniaelsen@gmail.com>
2025-03-13 11:42:55 -0700

    feat(synapse): update to synapse-cpp v1.0.0

--- ports/synapse/portfile.cmake
+++ ports/synapse/portfile.cmake
@@ -8,15 +8,15 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO sciencecorp/synapse-cpp
-	REF f8d8e7559ade11984d8127f99ae1e76e675fb596
-	SHA512 c5342fd44ef7f774897917b692a38cfb50cd8edbff36ff06357b5ea8ebf67f28b64aac747a651d3da9ee017b327a60d8ec57d979b3afd90e1dbbc9e88e2fd4b2
+	REF da0daa82a4b8db901e4603145f4a5aac78925077
+	SHA512 a6955142d35fbc3c67eed3053468113048ec5a8d186c29e74e9c26af03ae71377d20f0535dead266b5b89c99e879daf60cb7a1aa9faeff70d9a67797e75c67a8
 )
 
 vcpkg_download_distfile(API_ARCHIVE
-	URLS "https://github.com/sciencecorp/synapse-api/archive/62ddda4e36a3f0f3c4608e3a3c30100d9716e0dd.zip"
+	URLS "https://github.com/sciencecorp/synapse-api/archive/22f14a204e007fbbb7695aea84c344b41f5b47dc.zip"
 	FILENAME "synapse-api.zip"
 	SOURCE_BASE "synapse-api"
-	SHA512 80c64c89387edf87a8ec1927e36c731b38325a3dab9439d00281d52500eef7b3d61c25853e8879e5785a904a39f54a53c292ccae8fd7bae1b1cfce97798b91fc
+	SHA512 1d454948e589e48a8ffd8277d72426a2fb077789f62000f2361a177ded2892e733e410064f2ceadc46ad9f954081d9479b602c95a2d64db6e40617b813f0ccd2
 )
 
 vcpkg_extract_source_archive(
